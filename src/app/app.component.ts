f854705346279e7560d006caea4f1ad07bf43f9f
sapaev_n <snu5207824@icloud.com>
2023-05-05 13:43:03 +0500

    add function (change bg)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'samarCourse';
+
+  name:any='Nuriddin';
+  age = 22;
+  count: number = 0
+  color = ''
+  changeBg() {
+    const arr: string[] = ['#777','#123','#345']
+    document.body.style.background = `${arr[this.count]}`
+    this.color = arr[this.count]
+    this.count++
+    if(this.count === arr.length){
+      this.count = 0;
+    }
+  }
+
+  increse():void{
+    this.age++
+  }
+
+
 }
